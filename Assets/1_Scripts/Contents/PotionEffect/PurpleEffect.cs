e14dcf26cc5f5a2354bac80285bd44f5fa7fa92a
Inho <yeoinho75@gmail.com>
2024-03-25 00:00:34 +0900

    feat: Effect에 타이머 추가
    Effect 중심에 끝나는 시간을 알 수 있도록
    타이머 추가

--- Assets/1_Scripts/Contents/PotionEffect/PurpleEffect.cs
+++ Assets/1_Scripts/Contents/PotionEffect/PurpleEffect.cs
@@ -1,9 +1,21 @@
 using EnemyUI.BehaviorTree;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class PurpleEffect : MonoBehaviour
 {
+    private TextMeshPro textMesh;
+    private float duringTime = 5f;
+
+    private void OnEnable()
+    {
+        foreach (Transform child in transform)
+            if (child.GetComponent<TextMeshPro>() != null) 
+                textMesh = child.GetComponent<TextMeshPro>();
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<EnemyBT>() != null)
@@ -23,7 +35,14 @@ public class PurpleEffect : MonoBehaviour
 
     public IEnumerator TimerCoroutine()
     {
-        yield return new WaitForSeconds(5f);
+        float elapsedTime = 0f;
+        while (elapsedTime < duringTime)
+        {
+            elapsedTime += Time.deltaTime;
+            textMesh.text = (duringTime - elapsedTime).ToString("0.0");
+            yield return null;
+        }
+
         Destroy(this.gameObject);
     }
 
