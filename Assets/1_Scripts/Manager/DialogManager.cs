76a5368bf4fe3ff11accb674429ed06056ae48fa
Inho <yeoinho75@gmail.com>
2024-04-05 16:38:51 +0900

    feat: Event Select
    이벤트 진행 중 대화창에서 분기를 선택할 수 있도록 구현

--- Assets/1_Scripts/Manager/DialogManager.cs
+++ Assets/1_Scripts/Manager/DialogManager.cs
@@ -6,17 +6,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Localization.Settings;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 
 public class DialogManager : MonoBehaviour
 {
+    [Header("Dialog")]
     [SerializeField] private GameObject dialogBox;
     [SerializeField] private TextMeshProUGUI dialogActorName;
     [SerializeField] private Image dialogActorSprite;
     [SerializeField] private TextMeshProUGUI dialogText;
 
+
+    [Header("Select")]
+    [SerializeField] private GameObject selectsParent;
+    // Height 는 y: 50단위
+    // select 됐을때에는 x : -20
+    // selector는 y따라가고 -2.5 (offset)
+    [SerializeField] private GameObject selectTextPrefab;
+    [SerializeField] private GameObject selectorImage;
+
     private DialogEventInfo eventInfo;
+    private List<GameObject> selects;
+
+    private const float SELECTEHEIGHT = 50f;
+    private const float SELECTEDOFFSET = 30f;
+
 
     private bool dialogInProgress = false;
+    private bool selectinProgress = false;
     private bool isTyping = false;
 
     private List<string> dialogKeys = new List<string>();
@@ -25,7 +43,7 @@ public class DialogManager : MonoBehaviour
     private int curDialogueIndex = 0;
     private int curLineIndex = 0;
 
-
+    private int curSelectedIndex = 0;
 
     private float dialogHidePosY = -1000;
     private float dialogRevealPosY = 0;
@@ -34,7 +52,7 @@ public class DialogManager : MonoBehaviour
     private void Start()
     {
 
-
+        selectorImage.SetActive(false);
         dialogBox.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, dialogHidePosY, 0);
     }
 
@@ -43,7 +61,8 @@ public class DialogManager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if (!dialogInProgress) SetEvent(1);
+            if (!dialogInProgress) SetEvent(0);
+            else UnsetEvent();
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && dialogInProgress)
@@ -63,9 +82,21 @@ public class DialogManager : MonoBehaviour
                 }
                 else
                 {
-                    // 여기 이벤트 선택지 생성하면됨
+                    if (!selectinProgress)
+                    {
+                        MakeSelection();
+                        // 여기 이벤트 선택지 생성하면됨
+                    }
                 }
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && selectinProgress)
+        {
+            OnUpArrow();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && selectinProgress) {
+            OnDownArrow();
         }
     }
 
@@ -101,7 +132,7 @@ public class DialogManager : MonoBehaviour
 
     private void UnsetEvent()
     {
-        dialogBox.GetComponent<RectTransform>().DOAnchorPosY(dialogHidePosY, 0.6f);
+        dialogBox.GetComponent<RectTransform>().DOAnchorPosY(dialogHidePosY, 0.6f).SetEase(Ease.InOutElastic);
         dialogInProgress = false;
     }
 
@@ -146,4 +177,45 @@ public class DialogManager : MonoBehaviour
         isTyping = false;
     }
 
+    private void MakeSelection()
+    {
+
+        selectinProgress = true;
+        selectorImage.SetActive(true);
+        selectorImage.GetComponent<RectTransform>().anchoredPosition = new Vector2(-10f, 0);
+
+        selects = new List<GameObject>();
+
+        for (int i = 0; i < eventInfo.eventSelectCnt; i++)
+        {
+            selects.Add(Instantiate(selectTextPrefab, selectsParent.transform));
+            selects[i].GetComponent<RectTransform>().anchoredPosition = new Vector3(0, SELECTEHEIGHT * (eventInfo.eventSelectCnt - i - 1), 0);
+            selects[i].GetComponent<TextMeshProUGUI>().text = LocalizationSettings.StringDatabase.GetLocalizedString("DialogInfo", 
+                 eventInfo.eventName + "_S" + i.ToString());
+        }
+        
+        curSelectedIndex = 0;
+        SelectIndex(0);
+        // 자동으로 첫번째 선택
+
+    }
+
+    private void OnUpArrow()
+    {
+        SelectIndex((curSelectedIndex - 1) < 0 ? eventInfo.eventSelectCnt -1 : (curSelectedIndex - 1));
+    }
+    private void OnDownArrow()
+    {
+        SelectIndex((curSelectedIndex + 1) % eventInfo.eventSelectCnt);
+    }
+
+    private void SelectIndex(int ind)
+    {
+        selects[curSelectedIndex].GetComponent<RectTransform>().DOAnchorPosX(0, 0.5f);
+
+        selectorImage.GetComponent<RectTransform>().anchoredPosition = selects[ind].GetComponent<RectTransform>().anchoredPosition - new Vector2(0, 5f);
+        selects[ind].GetComponent<RectTransform>().DOAnchorPosX(-SELECTEDOFFSET, 0.5f);
+
+        curSelectedIndex = ind;
+    }
 }
