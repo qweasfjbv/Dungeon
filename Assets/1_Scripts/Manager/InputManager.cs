3783a1116322a00105404fe303d4192ecd3b8e2c
YeoInho <yeoinho75@gmail.com>
2024-05-13 14:05:18 +0900

    feat: Treasure Expanded UI

--- Assets/1_Scripts/Manager/InputManager.cs
+++ Assets/1_Scripts/Manager/InputManager.cs
@@ -17,23 +17,23 @@ public class InputManager
     private bool dialogBlock = false;
     private bool escBlock = false;
     private bool invenBlock = false;
-    private bool treasureBlock = false;
 
     public bool DialogBlock { get => dialogBlock; set => dialogBlock = value; }
     public bool EscBlock { get => escBlock; set => escBlock = value; }
     public bool InvenBlock { get => invenBlock; set => invenBlock = value; }
-    public bool TreasureBlock{ get => treasureBlock; set => treasureBlock = value; }
 
 
     public void OnUpdate()
     {
         if (Input.anyKey == false) return;
 
-        if (cardAction != null && !escBlock && !dialogBlock && !invenBlock && !treasureBlock) cardAction.Invoke();
-        if (dialogAction != null && !escBlock && !invenBlock && !treasureBlock) dialogAction.Invoke();
-        if (escAction != null && !dialogBlock && !invenBlock && !treasureBlock) escAction.Invoke();
-        if (invenAction != null && !dialogBlock && !escBlock && !treasureBlock) invenAction.Invoke();
-        if (treasureAction != null && !dialogBlock && !escBlock && !invenBlock) treasureAction.Invoke();
+        if (cardAction != null && !escBlock && !dialogBlock && !invenBlock) {
+            cardAction.Invoke();
+            treasureAction.Invoke();
+        }
+        if (dialogAction != null && !escBlock && !invenBlock) dialogAction.Invoke();
+        if (escAction != null && !dialogBlock && !invenBlock) escAction.Invoke();
+        if (invenAction != null && !dialogBlock && !escBlock) invenAction.Invoke();
 
     }
 
--- Assets/1_Scripts/UI/TreasureController.cs
+++ Assets/1_Scripts/UI/TreasureController.cs
@@ -1,15 +1,38 @@
 using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TreasureController : MonoBehaviour
 {
     [SerializeField] private RectTransform treasureList;
+    [SerializeField] private Button treasureShowButton;
 
-    private bool isTListOpen;
+    [SerializeField] private Vector2 padding;
+    [SerializeField] private Vector2 spacing;
+
+    [Space]
+    [Header("Expand Duration")]
+    [SerializeField] private float collapseDuration;
+    [SerializeField] private float expandDuration;
+    [SerializeField] private Ease expandEase;
+    [SerializeField] private Ease collapseEase;
+
+    [Space]
+    [Header("Fade Duration")]
+    [SerializeField] private float fadeInDuration;
+    [SerializeField] private float fadeOutDuration;
+
+    private List<TreasureLIstItem> treasureItems = new List<TreasureLIstItem>();
+    private int itemCount;
+    private Vector2 mainButtonPos;
+    private bool isExtended;
 
     private void Awake()
     {
-        isTListOpen = false;
+        mainButtonPos = Vector2.zero;
+        itemCount = treasureList.childCount;
+        resetPosition();
     }
 
     private void Start()
@@ -17,26 +40,60 @@ public class TreasureController : MonoBehaviour
         Managers.Input.treasureAction -= OnKeyboard;
         Managers.Input.treasureAction += OnKeyboard;
 
-        treasureList.localScale = new Vector3(0f, 1f, 1f);
+        treasureShowButton.onClick.RemoveListener(Toggle);
+        treasureShowButton.onClick.AddListener(Toggle);
+
     }
 
     private void OnKeyboard()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-
-            if (Managers.Input.TreasureBlock) // 열려있음. 닫는 부분
-            {
-                Managers.Input.TreasureBlock = false;
-                treasureList.DOScaleX(0f, 0.5f);
-            }
-            else
-            {
-                treasureList.DOScaleX(1f, 0.5f);
-                Managers.Input.TreasureBlock = true;
-            }
+            Toggle();
         }
     }
 
+
+    private void Toggle()
+    {
+
+
+        if (isExtended) // 열려있음. 닫는 부분
+        {
+            isExtended = false;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                treasureItems[i].rect.DOAnchorPos(mainButtonPos, collapseDuration).SetEase(collapseEase);
+                treasureItems[i].image.DOFade(0f, fadeOutDuration);
+            }
+        }
+        else
+        {
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                treasureItems[i].rect.DOAnchorPos(mainButtonPos + spacing * (i + 1) + padding, expandDuration).SetEase(expandEase);
+                treasureItems[i].image.DOFade(1f, fadeInDuration);
+            }
+
+            isExtended = true;
+        }
+    }
+    private void resetPosition()
+    {
+        for (int i = 0; i < itemCount; i++)
+        {
+            treasureItems.Add(treasureList.GetChild(i).GetComponent<TreasureLIstItem>());
+            treasureItems[i].rect.anchoredPosition3D = mainButtonPos;
+        }
+
+        isExtended = false;
+    }
+
+    public void AddItemList(int id)
+    {
+
+    }
 
 }
